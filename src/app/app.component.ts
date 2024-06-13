a8c547b3d7b7c23e547e8fdaba1aae9c6aeec7a2
Ramsey Louder <RamseyLouder@gmail.com>
2024-06-13 09:05:26 -0400

    Chirpuspizzalab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,67 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'chirpuspizzalab';
+  title:string = "Welcome To Chirpus Pizza";
+
+  Menu:MenuItem[] = [
+
+   {
+        name: "Pepperoni Pizza",
+        category: "Hot food",
+        price: 5.50,
+        vegetarian: false,
+        soldOut: false
+
+   },
+   {
+    name: "Cheese Pizza",
+    category: "Hot food",
+    price: 5.00,
+    vegetarian: true,
+    soldOut: false
+
+},
+{
+  name: "Buffalo Wings",
+  category: "Hot food",
+  price: 10.00,
+  vegetarian: false,
+  soldOut: false
+
+},
+{
+  name: "Garlic Knots",
+  category: "Hot food",
+  price: 7.00,
+  vegetarian: true,
+  soldOut: true
+
+},
+{
+  name: "Hoagie",
+  category: "Cold food",
+  price: 8.00,
+  vegetarian: false,
+  soldOut: false
+
+},
+{
+  name: "Garden Salad",
+  category: "Cold food",
+  price: 8.00,
+  vegetarian: true,
+  soldOut: false
+
+},
+{
+  name: "Soda",
+  category: "Drinks",
+  price: 3.00,
+  vegetarian: true,
+  soldOut: false
+
+},
+  ]
+
+  
 }
---
+++ src/app/models/menu-item.ts
@@ -0,0 +1,9 @@
+export interface MenuItem {
+    name:string;
+    category: string;
+    price: number;
+    vegetarian: boolean;
+    soldOut: boolean;
+
+    
+}
